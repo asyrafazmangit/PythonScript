996339b6b705329da4f77f6f15085ce230783fd1
asyraf.azman <asyraf.azman@uberfusion.com>
2024-12-09 16:23:31 +0800

    Install the Python and Dependencies

---
+++ pip-dependencies.sh
@@ -0,0 +1,24 @@
+#Install boto3 using pip
+pip3 install boto3
+
+#Verify the installation
+python3 -m pip show boto3
+
+#Solution: Use a Virtual Environment
+#Create a virtual environment: Run the following command to create a virtual environment named venv in your project directory:
+python3 -m venv venv
+
+#Activate the virtual environment: 
+source venv/bin/activate
+
+#Install boto3 inside the virtual environment
+pip install boto3
+
+#Install pandas in the Virtual Environment
+source venv/bin/activate
+
+#Install pandas
+pip install pandas
+
+#Verify the pandas
+pip show pandas
